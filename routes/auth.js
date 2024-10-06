babaf3bbda71549f59f094f5a665f6d82ce0c4e8
SwathiReddy08 <swathireddyaus0809@gmail.com>
2024-10-07 02:46:33 +0530

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+const router = express.Router();
+
+// Login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user && await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        res.json({ token });
+    } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+    }
+});
+
+export default router;
